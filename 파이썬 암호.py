76e8ea2f84a71bd8ceef203167e068b68cf92ff0
SuNyeon-Yum <85491736+SuNyeon-Yum@users.noreply.github.com>
2022-04-01 11:41:57 +0900

    Add files via upload
    Python Password (2022_04_01)

---
+++ 파이썬 암호.py
@@ -0,0 +1,27 @@
+code1 = ("EULJI UNIVERSITY")
+code2 = ("IN SEONGNAM")
+
+print(type(code1))
+print(type(code2))
+
+
+str=''
+
+print()
+
+for i in code1:
+    if ord(i) >= ord ('A') and ord(i) <=ord('Z'):
+        print(chr(ord(i)-2),end='I')
+        str=str+chr(ord(i)+1)
+
+
+print()
+print()
+
+for i in code2:
+    if ord(i) >= ord ('A') and ord(i) <=ord('Z'):
+        print(chr(ord(i)-2),end='W')
+        str=str+chr(ord(i)+1)
+
+print()
+
