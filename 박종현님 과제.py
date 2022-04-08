ffda3c706b578539c0a530d72c190d93b0bb7735
SuNyeon-Yum <85491736+SuNyeon-Yum@users.noreply.github.com>
2022-04-08 09:05:19 +0900

    Add files via upload

---
+++ 박종현님 과제.py
@@ -0,0 +1,18 @@
+sentence = 'I hope you have a good day'
+
+result = len(sentence.split())
+
+print("이 문장에는 " + str(result) + "개의 단어가 있습니다")
+
+print('----------')
+
+print('----------')
+
+print('----------')
+
+str = ('Today I learned')
+
+str.split()
+
+print(str.split())
+
